9eb80af22088c04c6e0f8fb908dcac1c890b8ee2
DaríoOjeda <darioojeda_1995@hotmail.com>
2020-10-02 19:44:47 +0200

    hasta el treatment(hay errata en surname)

--- app/src/main/java/es/iessaladillo/pedrojoya/greet/MainActivity.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/greet/MainActivity.java
@@ -1,8 +1,13 @@
 package es.iessaladillo.pedrojoya.greet;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        setUpViews();
+    }
+
+    private void setUpViews(){
+        ImageView icono = ActivityCompat.requireViewById(this, R.id.iconperson);
+        RadioButton mister = ActivityCompat.requireViewById( this, R.id.senor);
+        RadioButton mrs = ActivityCompat.requireViewById( this, R.id.senora);
+        RadioButton ms = ActivityCompat.requireViewById(this, R.id.senorita);
+
+
+        mister.setOnClickListener(v ->
+                icono.setImageDrawable(getDrawable(R.drawable.ic_mr))
+        );
+
+        mrs.setOnClickListener(v ->
+            icono.setImageDrawable(getDrawable(R.drawable.ic_mrs))
+                );
+        ms.setOnClickListener(v ->
+                icono.setImageDrawable(getDrawable(R.drawable.ic_ms))
+        );
+
+
+
+
+        mister.setChecked(true);
+
+
     }
 
 }
